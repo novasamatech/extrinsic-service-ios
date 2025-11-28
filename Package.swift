52ff510a2442d10c86e64c33a0ae61129bd690cd
ERussel <emkil.russel@gmail.com>
2025-11-28 17:55:50 +0100

    bump substrate

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             ]),
     ],
     dependencies: [
-        .package(url: "https://github.com/novasamatech/substrate-sdk-ios", exact: "5.5.0"),
+        .package(url: "https://github.com/novasamatech/substrate-sdk-ios", exact: "5.6.0"),
         .package(url: "https://github.com/novasamatech/Keystore-iOS", exact: "1.1.0"),
         .package(url: "https://github.com/novasamatech/metadata-shortener-ios", exact: "0.2.1"),
         .package(url: "https://github.com/novasamatech/logger-ios", exact: "0.0.1")
