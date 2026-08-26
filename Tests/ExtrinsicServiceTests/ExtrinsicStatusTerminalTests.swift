650686180c78e8da8658daffe9422329eec4b242
ERussel <emkil.russel@gmail.com>
2026-08-26 10:29:33 +0200

    fix tests

--- Tests/ExtrinsicServiceTests/ExtrinsicStatusTerminalTests.swift
+++ Tests/ExtrinsicServiceTests/ExtrinsicStatusTerminalTests.swift
@@ -4,30 +4,31 @@ import Foundation
 
 @Suite("Extrinsic Status Terminal Detection")
 struct ExtrinsicStatusTerminalTests {
-    private let hash = "0x69d25c4f2cfd1eeae88455679f435569024f2e723a403062929e3d4557a531b7"
+    private let blockHash = "0x69d25c4f2cfd1eeae88455679f435569024f2e723a403062929e3d4557a531b7"
+    private let extrinsicHash = "0x69d25c4f2cfd1eeae88455679f435569024f2e723a403062929e3d4557a542c8"
 
     private func update(_ status: RemoteExtrinsicStatus) -> ExtrinsicStatusUpdate {
-        ExtrinsicStatusUpdate(extrinsicHash: "0xext", extrinsicStatus: .onChain(status))
+        ExtrinsicStatusUpdate(extrinsicHash: extrinsicHash, extrinsicStatus: .onChain(status))
     }
 
     // MARK: - inBlock tracking (default, current behavior)
 
     @Test func inBlockTrackingIsTerminalAtInBlock() {
-        #expect(update(.inBlock(hash)).getTerminalBlockHash(trackingTill: .inBlock) == hash)
+        #expect(update(.inBlock(blockHash)).getTerminalBlockHash(trackingTill: .inBlock) == blockHash)
     }
 
     @Test func inBlockTrackingIsTerminalAtFinalized() {
-        #expect(update(.finalized(hash)).getTerminalBlockHash(trackingTill: .inBlock) == hash)
+        #expect(update(.finalized(blockHash)).getTerminalBlockHash(trackingTill: .inBlock) == blockHash)
     }
 
     // MARK: - finalized tracking (new behavior)
 
     @Test func finalizedTrackingIgnoresInBlock() {
-        #expect(update(.inBlock(hash)).getTerminalBlockHash(trackingTill: .finalized) == nil)
+        #expect(update(.inBlock(blockHash)).getTerminalBlockHash(trackingTill: .finalized) == nil)
     }
 
     @Test func finalizedTrackingIsTerminalAtFinalized() {
-        #expect(update(.finalized(hash)).getTerminalBlockHash(trackingTill: .finalized) == hash)
+        #expect(update(.finalized(blockHash)).getTerminalBlockHash(trackingTill: .finalized) == blockHash)
     }
 
     // MARK: - non-inclusion statuses are never terminal for either target
@@ -38,6 +39,6 @@ struct ExtrinsicStatusTerminalTests {
     }
 
     @Test func retractedIsNeverTerminal() {
-        #expect(update(.retracted(hash)).getTerminalBlockHash(trackingTill: .finalized) == nil)
+        #expect(update(.retracted(blockHash)).getTerminalBlockHash(trackingTill: .finalized) == nil)
     }
 }
